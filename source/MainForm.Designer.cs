60066a351d5d8905f57f1be32af19fedc6a28898
tarosay <tarosa.y@gmail.com>
2016-05-02 00:14:01 +0900

    First Commit

---
+++ source/MainForm.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace _2つのバイナリファイルを比較する
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(4, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(373, 319);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "2つのファイルをドラッグしてね。\r\nサイズの小さいほうが、サイズの大きなほうに含まれていないか探します。\r\n\r\n";
+            this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox1_DragDrop);
+            this.textBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBox1_DragEnter);
+            // 
+            // MainForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 343);
+            this.Controls.Add(this.textBox1);
+            this.Name = "MainForm";
+            this.Text = "2つのバイナリファイルを比較する";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainForm_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+
+    }
+}
+
---
+++ source/MainForm.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2つのバイナリファイルを比較する
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox1.AppendText("　ファイルをドラッグしてね。\r\n");            
+            textBox1.AppendText("　一番大きなサイズのファイルの中身を検索して、他のファイルと同じデータが格納されている場所を検索します。\r\n");
+        }
+
+        private void Hikaku(string[] files)
+        {
+            if (files.Length < 2)
+            {
+                textBox1.AppendText("2つ以上のファイルをドラッグしてください\r\n");
+            }
+
+            textBox1.AppendText("開始します\r\n");
+            Application.DoEvents();
+
+            try
+            {
+                //ファイルサイズ取得
+                string bigFile = files[0];
+                List<FileInfo> searchFiles = new List<FileInfo>();
+
+                FileInfo maxFile = new FileInfo(files[0]);
+                for (int i = 0; i < files.Length; i++)
+                {
+                    FileInfo file = new FileInfo(files[i]);
+                    searchFiles.Add(file);
+
+                    if (maxFile.Length < file.Length)
+                    {
+                        maxFile = file;
+                    }
+                }
+
+                textBox1.AppendText(Path.GetFileName(maxFile.Name) + " からデータを検索します。\r\n");
+
+
+
+                byte[] maxdat = null;
+                byte[] dat = null;
+
+                //バイナリを読み込みます
+                if (File.Exists(bigFile))
+                {
+                    maxdat = File.ReadAllBytes(maxFile.Directory + "\\" + maxFile.Name);
+                }
+
+
+                for (int k = 0; k < searchFiles.Count; k++)
+                {
+                    if (maxFile.Length == searchFiles[k].Length)
+                    {
+                        continue;
+                    }
+
+                    if (File.Exists(searchFiles[k].Directory + "\\" + searchFiles[k].Name))
+                    {
+                        dat = File.ReadAllBytes(searchFiles[k].Directory + "\\" + searchFiles[k].Name);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+
+
+
+                    int j = 0;
+                    for (int i = 0; i < maxdat.Length; i++)
+                    {
+                        if (maxdat[i] == dat[j])
+                        {
+                            j++;
+                            if (j >= dat.Length)
+                            {
+                                int add = i - j + 1;
+                                textBox1.AppendText("　　" + searchFiles[k].Name + " が、アドレス: " + add.ToString("X") + "～ に見つかりました。\r\n");
+                                j = 0;
+                            }
+                        }
+                        else
+                        {
+                            j = 0;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            textBox1.AppendText("検索は終了しました。\r\n");
+        }
+
+
+        #region //          ドラッグドロップ        //
+        private void MainForm_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void MainForm_DragDrop(object sender, DragEventArgs e)
+        {
+            // ドラッグ＆ドロップされたファイル
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            Hikaku(files);
+        }
+
+        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            // ドラッグ＆ドロップされたファイル
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            Hikaku(files);
+        }
+
+        private void textBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+        #endregion
+    }
+}
